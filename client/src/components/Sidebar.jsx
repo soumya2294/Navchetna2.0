a8d146e65dfe13e18d5d5949e124f4e12eae99b4
Sachin <imsachinkumar0723@gmail.com>
2026-09-15 12:50:51 +0530

    ADDED NAME

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -43,8 +43,8 @@ const Sidebar = ({ isOpen, closeSidebar }) => {
             onClick={closeSidebar}
           >
             <span className="sidebar-brand-text">
-              NAVCHETNA
-              <span className="brand-accent">2.0</span>
+              FIT
+              <span className="brand-accent">MATES</span>
             </span>
           </Link>
 
